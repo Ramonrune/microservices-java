72739fd9d434ded7144210a04a6bf2c3ef46c78e
Ramon <ramonrune@gmail.com>
2022-09-16 13:47:07 -0300

    first commit

---
+++ spring-boot-admin/src/test/java/br/com/alura/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springbootadmin;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
